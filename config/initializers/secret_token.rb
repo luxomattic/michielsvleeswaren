c5018dba75eae10bdb4d07321462f645b7f70ef0
Thomas Michiels <michiels.thomas@gmail.com>
2011-06-20 15:48:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Michielsvleeswaren::Application.config.secret_token = 'ded16f3d87084c04418630fb0d150dcfa7611dab3813d90bed6a6205397df81d09ef9ee9a997fac725c94b7c2223e47f47c9e109c9f2b607c6be7d25a65de29a'
