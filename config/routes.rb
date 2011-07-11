58a4c032dc95795dbd82e567bfbcff11c50b6077
Thomas Michiels <michiels.thomas@gmail.com>
2011-07-11 14:02:58 +0200

    done with singing in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Michielsvleeswaren::Application.routes.draw do
-  
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
+  match '/signup', :to => 'users#new'
+  
+  match '/signin', :to => 'sessions#new'
+  
+  match '/signout', :to => 'sessions#destroy'  
 
   root :to => 'pages#home'
   
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -95,6 +95,11 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /Welkom bij Michiels Vleeswaren/i
       end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
   end
 end
